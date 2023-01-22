0a8d3c2272e346526ccc04720b1c347a43b53ea4
Dave Slaughter <26726004+dave-slaughter@users.noreply.github.com>
2023-01-22 06:37:19 +0000

    move .htaccess backup in with wp-config

--- bin/config-backup.sh
+++ bin/config-backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Backup the WordPress config to the south-lacrosse/wordpress repo if changed
+# Backup the WordPress config & .htaccess to the south-lacrosse/wordpress repo if changed
 
-echo 'Backing up wp-config.php (if needed)'
-COMMAND='cp ../../../wp-config.php .'
+echo 'Backing up wp-config.php and .htaccess (if needed)'
+COMMAND='cp ../../../{wp-config.php,.htaccess} .'
 source $(dirname "$0")/wp-git-repo.sh
