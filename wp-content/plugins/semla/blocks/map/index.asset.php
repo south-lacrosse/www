22ce21524e1f16bea139d787a5a54c7b4d33c73a
Dave Slaughter <26726004+dave-slaughter@users.noreply.github.com>
2023-02-14 10:22:25 +0000

    don't use JSON from another block

--- wp-content/plugins/semla/blocks/map/index.asset.php
+++ wp-content/plugins/semla/blocks/map/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => '4516e1ffb4040eb892f7');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => '8b8fd6a5a5e9f320d4dd');
