921bbf3b9ddea659854d1c32a27a91ff08f565cc
Dave Slaughter <26726004+dave-slaughter@users.noreply.github.com>
2023-02-16 22:10:15 +0000

    Stop users adding block templates in the editor

--- wp-content/themes/lax/functions.php
+++ wp-content/themes/lax/functions.php
@@ -39,6 +39,9 @@ add_action('after_setup_theme', function() {
 	// don't add new global styles added in WP 5.8 for blocks
 	remove_action( 'wp_enqueue_scripts', 'wp_enqueue_global_styles' );
 
+	// Don't allow users to add new block templates in the editor
+	remove_theme_support( 'block-templates' );
+
 	// Let WordPress manage the document title.
 	add_theme_support('title-tag');
 
