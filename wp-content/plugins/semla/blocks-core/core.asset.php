1c8fcf8c3da5b6d8414ce75621094f345a7ef561
Dave Slaughter <26726004+dave-slaughter@users.noreply.github.com>
2023-05-26 09:51:07 +0100

    don't restrict social/embed variations

--- wp-content/plugins/semla/blocks-core/core.asset.php
+++ wp-content/plugins/semla/blocks-core/core.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-primitives'), 'version' => 'bbd776f1fe2ac35247fa');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-primitives'), 'version' => 'f6ef2dd81a658f3d37a8');
