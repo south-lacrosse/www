5a1bba96b6c97e3a6a473a6e78a8634b45b3d55a
Dave Slaughter <26726004+dave-slaughter@users.noreply.github.com>
2023-08-16 17:03:33 +0100

    map block fixes
    - update for new Google maps importLibrary
    - get address from parent Location block
    - fix map height in modal

--- wp-content/plugins/semla/blocks-core/core.asset.php
+++ wp-content/plugins/semla/blocks-core/core.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-primitives'), 'version' => '057eeb74679e61ebd59b');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-primitives'), 'version' => '55eddb216e3029301bad');
--- wp-content/plugins/semla/blocks/map/index.asset.php
+++ wp-content/plugins/semla/blocks/map/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => '6f24016a69bbbb515d11');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => 'e3ca5f4004e689fe2d96');
