c28be893b5be4935857d9a4fd5247c727fffd75f
Dave Slaughter <26726004+dave-slaughter@users.noreply.github.com>
2023-09-20 19:22:10 +0100

    refactor block editor css, remove workarounds
    - where components are styled by the theme's css it makes more sense to
      have the editor styles in the theme too
    - remove css workarounds for block editor

--- wp-content/plugins/semla/blocks/core/index.asset.php
+++ wp-content/plugins/semla/blocks/core/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-primitives'), 'version' => '71c15602e4478033af07');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-primitives'), 'version' => 'f3f509b51397a3a02541');
