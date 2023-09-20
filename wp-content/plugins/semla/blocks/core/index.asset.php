056dda44da5f7ad34f456651391b11aa207641d7
Dave Slaughter <26726004+dave-slaughter@users.noreply.github.com>
2023-09-20 11:38:49 +0100

    remove ordered list settings for custom classes
    - WordPress has settings for e.g. type="a" for lower-alpha

--- wp-content/plugins/semla/blocks/core/index.asset.php
+++ wp-content/plugins/semla/blocks/core/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-primitives'), 'version' => '1e0bb020ab021054057b');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-primitives'), 'version' => '71c15602e4478033af07');
