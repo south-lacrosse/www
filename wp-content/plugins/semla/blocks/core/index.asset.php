de2cd36157871a3cb629ed06c8d22553d93cf1f3
Dave Slaughter <26726004+dave-slaughter@users.noreply.github.com>
2023-09-21 16:59:27 +0100

    remove no-print from paragraph block
    - can be on any block, so just added to help pages

--- wp-content/plugins/semla/blocks/core/index.asset.php
+++ wp-content/plugins/semla/blocks/core/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-primitives'), 'version' => 'f3f509b51397a3a02541');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-primitives'), 'version' => '21387357b8943d5b12e8');
