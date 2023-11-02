7bdc2e9eb389392e0aceafe0be7c9d853eb3023c
Dave Slaughter <26726004+dave-slaughter@users.noreply.github.com>
2023-11-02 14:09:34 +0000

    improve Net_Util
    - allow whatever encoding cURL allows
    - curl_close does nothing on PHP 8+ so remove

--- wp-content/plugins/semla/core/Utils/Net_Util.php
+++ wp-content/plugins/semla/core/Utils/Net_Util.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Utility methods for network stuff called from multiple places
- * 
+ *
  * If we will be doing multiple curl requests per session then this class
  * should be rewritten to reuse the curl handle as that is much more
  * efficient.
@@ -16,36 +16,29 @@ class Net_Util {
 	public static function get_url($url, $expected_content_type = '') {
 		$ch = self::get_curl($url);
 		if (is_wp_error($ch)) $ch;
- 
+
 		curl_setopt($ch, CURLOPT_HEADER, 0); // no headers
 		// Fail the cURL request if response code = 400 (like 404 errors)
 		curl_setopt($ch, CURLOPT_FAILONERROR, true);
-		curl_setopt($ch, CURLOPT_ENCODING, 'gzip,deflate');
- 
+		curl_setopt($ch, CURLOPT_ENCODING, ''); // all supported encodings
+
 		$data = curl_exec($ch);
- 
+
 		if (curl_errno($ch)) {
-			$err = new \WP_Error('curl', 'cURL error ' . curl_errno($ch) . ': ' . curl_error($ch));
-			curl_close($ch);
-			return $err;
+			return new \WP_Error('curl', 'cURL error ' . curl_errno($ch) . ': ' . curl_error($ch));
 		}
 		$resp = curl_getinfo($ch, CURLINFO_RESPONSE_CODE);
 		if ($resp != 200) {
-			$err = new \WP_Error('curl', "invalid HTTP response code: $resp");
-			curl_close($ch);
-			return $err;
+			return new \WP_Error('curl', "invalid HTTP response code: $resp");
 		}
 		if ($expected_content_type) {
 			$content_type = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
 			if ($expected_content_type !== $content_type) {
-				$err = new \WP_Error('curl_unexpected_response_type',
-					'Unexpected content type getting remote content.' . 
+				return new \WP_Error('curl_unexpected_response_type',
+					'Unexpected content type getting remote content.' .
 					" Expected $expected_content_type, received $content_type, URL $url");
-				curl_close($ch);
-				return $err;
 			}
 		}
-		curl_close($ch);
 		return $data;
 	 }
 
@@ -76,7 +69,7 @@ class Net_Util {
 
 	 /**
 	 * See if a remote URL exists
-	 * 
+	 *
 	 * @return mixed true if exists, false if not, string if error
 	 */
 	public static function url_exists($url) {
@@ -87,12 +80,9 @@ class Net_Util {
 
 		curl_exec($ch);
 		if (curl_errno($ch)){   // should be 0
-			$err = new \WP_Error('curl', 'cURL error ' . curl_errno($ch) . ': ' . curl_error($ch));
-			curl_close($ch);
-			return $err;
+			return new \WP_Error('curl', 'cURL error ' . curl_errno($ch) . ': ' . curl_error($ch));
 		}
 		$code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-		curl_close($ch);
 		if ($code == 200) return true;
 		if ($code == 404) return false;
 		return new \WP_Error('curl', "Failed: http error code: $code url: $url");
