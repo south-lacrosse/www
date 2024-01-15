8c7818e769ca07804247771d78c979c268be11d5
Dave Slaughter <26726004+dave-slaughter@users.noreply.github.com>
2024-01-15 15:10:08 +0000

    correct/add removed calendars

--- wp-content/plugins/semla/core/Rest/team-removed-calendars.php
+++ wp-content/plugins/semla/core/Rest/team-removed-calendars.php
@@ -14,8 +14,9 @@ return [
 	'Epsom' => 1,
 	'Hillcroft B' => 1,
 	'Hitchin A' => 1,
-	'London_Univ.' => 1,
+	'London Univ.' => 1,
 	'Reading Wildcats 2' => 1,
 	'Plymouth City' => 1,
 	'Plymouth Univ.' => 1,
+	'Southampton' => 1,
 ];
