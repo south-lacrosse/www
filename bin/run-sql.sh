e4dcd07f415a1c32bcab6f9ae92c5662d87f8a5d
Dave Slaughter <26726004+dave-slaughter@users.noreply.github.com>
2024-01-18 13:14:46 +0000

    improve SQL script run-sql.sh
    - pass args to mysql
    - add ability to pass query string directly

--- bin/run-sql.sh
+++ bin/run-sql.sh
@@ -1,42 +1,85 @@
 #!/bin/bash
 
 # Run arbitrary SQL using the credentials from wp-config.php
-# If there are no args then will run the mysql client, leaving it open
-# If there are args then:
-#    arg 1 is an SQL file to run, which will be unziped if needed
-#    the user will be prompted to confirm unless 2nd arg is "-y"
 
-if [[ $# -gt 2 || ( $# -eq 2 && "$2" != '-y' ) ]]; then
-	echo "Usage: $0 [sql-file [-y]]"
+# If there are no args then will run the MySQL client, leaving it open
+
+# If there are args then they must be (any order):
+#    either:
+#       an SQL file to run, which will be unziped if needed
+#       -e "query"
+#
+#    -y to stop the default confirmation prompt
+#    optional flags which are passed to MySQL
+
+# Useful MySQL flags are:
+# -t - table display. Default batch mode (not -e flag) is to show tab separated
+#      result, not the nice table effect
+# -v, -vv, -vvv - verboseness, -vv will display each query and results
+
+function usage() {
+	echo "Usage: $(basename "$0") [(sql-file|-e 'sql query') [-y] [-mysql-arg...]]"
 	exit 1
-fi
+}
+
 if [[ $# -gt 0 ]]; then
-	if [[ ! -f "$1" ]] ; then
-		echo "File $1 does not exist."
-		exit 1
+	has_args=true
+	flags=()
+	while [[ $# -gt 0 ]]; do
+		case $1 in
+			-y)
+				no_confirm=true
+				;;
+			-e)
+				shift
+				if [[ $# -eq 0 || $1 = \-* || -n "$file" || -n "$query" ]]; then usage; fi
+				query=$1
+				;;
+			-*)
+				flags+=( $1 )
+				;;
+			*)
+				if [[ -n "$file" || -n "$query" ]]; then usage; fi
+				file=$1
+				;;
+		esac
+		shift
+	done
+	if [[ -z "$query" ]]; then
+		if [[ -z "$file" ]]; then usage; fi
+		if [[ ! -f "$file" ]] ; then
+			echo "File $file does not exist."
+			exit 1
+		fi
+		file=$(realpath "$file")
 	fi
-	file=$(realpath "$1")
 fi
 
 cd $(dirname "$0")
 source ./db-creds.sh
 echo -e "Running SQL on \e[93m$(grep "^define.*'WP_SITEURL" ../wp-config.php | sed "s/.*https:\/\/\([^']*\).*/\1/")\e[0m"
 
-if [[ $# -eq 0 ]]; then
+if [ "$has_args" != true ]; then
 	mysql --defaults-extra-file=.my.cnf
 	exit
 fi
 
-if [[ "$2" != '-y' ]]; then
-	read -p "Are you sure you want to run the SQL in $file? <y/N> " prompt
+if [ "$no_confirm" != true ]; then
+	if [[ -n "$query" ]]; then
+		read -p "Are you sure you want to run the SQL query \"$query\"? <y/N> " prompt
+	else
+		read -p "Are you sure you want to run the SQL in $file? <y/N> " prompt
+	fi
 	if [[ $prompt != "y" && $prompt != "Y" ]] ; then
 		echo 'Run SQL terminated by user.'
 		exit
 	fi
 fi
 
-if [[ "$file" =~ \.gz$ ]]; then
-	gunzip < "$file" | mysql --defaults-extra-file=.my.cnf
+if [[ -n "$query" ]]; then
+	mysql --defaults-extra-file=.my.cnf -e "$query" "${flags[@]}"
+elif [[ "$file" =~ \.gz$ ]]; then
+	gunzip < "$file" | mysql --defaults-extra-file=.my.cnf "${flags[@]}"
 else
-	mysql --defaults-extra-file=.my.cnf < "$file"
+	mysql --defaults-extra-file=.my.cnf "${flags[@]}" < "$file"
 fi
