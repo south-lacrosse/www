307cfb7cd4326f575ea7d2d7ae22100742b8d43f
Dave Slaughter <26726004+dave-slaughter@users.noreply.github.com>
2024-02-04 18:42:37 +0000

    load fixtures/tables from lacrosseplay api
    - add setting to update from either fixtures or lacrosseplay
    - add new Club Alias list table to translate club names

--- wp-content/plugins/semla/core/CLI/init.php
+++ wp-content/plugins/semla/core/CLI/init.php
@@ -1,6 +1,10 @@
 <?php
 add_action( 'cli_init', function() {
-	WP_CLI::add_command( 'fixtures', \Semla\CLI\Fixtures_Command::class );
+	if (get_option('semla_fixtures_source') === 'lacrosseplay') {
+		WP_CLI::add_command( 'fixtures', \Semla\CLI\Lacrosse_Play_Fixtures_Command::class );
+	} else {
+		WP_CLI::add_command( 'fixtures', \Semla\CLI\Fixtures_Command::class );
+	}
 	WP_CLI::add_command( 'history', \Semla\CLI\History_Command::class );
 	WP_CLI::add_command( 'monitor', \Semla\CLI\Monitor_Command::class );
 	WP_CLI::add_command( 'purge', \Semla\CLI\Purge_Command::class );
