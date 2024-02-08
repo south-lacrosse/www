3554f0385bc17dc789e2238b7731d624efba104c
Dave Slaughter <26726004+dave-slaughter@users.noreply.github.com>
2024-02-08 14:01:22 +0000

    add eventTypes param for Google calendar
    - use new option added to the API
    - refactor code

--- wp-content/plugins/semla/js/cal.js
+++ wp-content/plugins/semla/js/cal.js
@@ -13,6 +13,135 @@
 			'_'
 		);
 
+	// Load the JavaScript client library and invoke the loadCalendar function afterwards.
+	gapi.load('client', loadCalendar);
+
+	function loadCalendar() {
+		gapi.client
+			.init({
+				apiKey: window.semla.gapi,
+				discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest'],
+			})
+			.then(fetchEvents)
+			.then(onSuccess, onError);
+	}
+
+	function fetchEvents() {
+		// Events: list API docs: https://developers.google.com/calendar/v3/reference/events/list
+		return gapi.client.calendar.events.list({
+			calendarId: window.semla.cid,
+			eventTypes: ['default'],
+			timeZone: 'Europe/London',
+			singleEvents: true,
+			timeMin: new Date().toISOString(), //gathers only events not happened yet
+			// maxResults: 20,
+			orderBy: 'startTime',
+			fields:
+				'items(summary,start,end' +
+				(window.semla.calEnhanced ? ',description,location' : '') +
+				')',
+			// ,htmlLink
+		});
+	}
+
+	function onSuccess(response) {
+		var events = response.result.items;
+		if (!events || events.length === 0) {
+			setContent('<p>No pending events.</p>');
+			return;
+		}
+
+		var calendarRows = ['<dl class="calendar">'];
+		var lastYearMonth = '';
+		events.forEach(function (entry) {
+			var startDate = entry.start.date
+				? entry.start.date
+				: entry.start.dateTime.substring(0, 10);
+			var yearMonth = startDate.substring(0, 8);
+			if (yearMonth !== lastYearMonth) {
+				calendarRows.push(
+					'<dt>' +
+						months[parseInt(yearMonth.substring(5, 7), 10) - 1] +
+						' ' +
+						yearMonth.substring(0, 4) +
+						'</dt>'
+				);
+				lastYearMonth = yearMonth;
+			}
+			var day = parseInt(startDate.substring(8, 10), 10);
+			var startDateDate = new Date(startDate);
+			var line = days[startDateDate.getDay()] + ' ' + day + ordinal(day);
+			var endDate;
+			if (entry.start.dateTime) {
+				line += ', ';
+				var startTime = parseTime(entry.start.dateTime);
+				var endTime = parseTime(entry.end.dateTime);
+				endDate = entry.end.dateTime.substring(0, 10);
+				if (startDate === endDate) {
+					if (startTime.ampm === endTime.ampm) {
+						startTime.ampm = '';
+					}
+					line += startTime.time + startTime.ampm + '-' + endTime.time + endTime.ampm;
+				} else {
+					line +=
+						startTime.time +
+						startTime.ampm +
+						' - ' +
+						formatEndDate(startDate, endDate) +
+						', ' +
+						endTime.time +
+						endTime.ampm;
+				}
+			} else {
+				var date = new Date(entry.end.date);
+				// subtract 1 day, or 24 * 60 * 60 * 1000 seconds
+				date.setTime(date.getTime() - 86400000);
+				endDate = date.toISOString().split('T')[0];
+				if (endDate !== startDate) {
+					line += ' - ' + formatEndDate(startDate, endDate);
+				}
+			}
+			var summary = entry.summary;
+			var mark = '';
+			if (window.semla.calEnhanced) {
+				var split = summary.split(':');
+				if (split.length > 1) {
+					summary = split[0].trim();
+					mark = ' <mark class="tag';
+					var tag = split[1].trim();
+					if (window.semla.calTags && window.semla.calTags[tag]) {
+						mark += ' semla__tag' + window.semla.calTags[tag];
+					}
+					mark += '">' + tag + '</mark>';
+				}
+			}
+			// entry.description and location can only be set if we are in enhanced mode
+			if (entry.description) {
+				var url = extractLink(entry.description);
+				if (url) {
+					summary = '<a href="' + url + '">' + summary + '</a>';
+				}
+			} else {
+				summary = '<b>' + summary + '</b>';
+			}
+			line += ' - ' + summary;
+			if (entry.location) {
+				line += ' ' + entry.location;
+			}
+			line += mark;
+			calendarRows.push('<dd>' + line + '</dd>');
+		});
+		calendarRows.push('</dl>');
+		setContent(calendarRows.join(''));
+	}
+
+	function onError(reason) {
+		console.log(reason);
+		setContent('<p>Error: ' + reason.result.error.message + '</p>');
+	}
+
+	// Utility functions =======================================================
+
 	function ordinal(number) {
 		if (parseInt((number % 100) / 10, 10) === 1) {
 			// 10th,11th,12th etc
@@ -71,7 +200,7 @@
 		return found ? found[0] : false;
 	}
 
-	function replaceDiv(str) {
+	function setContent(str) {
 		var cal = document.getElementById('semla_cal');
 		if (!cal) {
 			console.log('Error: no div with id semla_cal');
@@ -79,126 +208,4 @@
 			cal.innerHTML = str;
 		}
 	}
-
-	function calendar() {
-		gapi.client
-			.init({
-				apiKey: window.semla.gapi,
-				discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest'],
-			})
-			.then(function () {
-				// Events: list API docs: https://developers.google.com/calendar/v3/reference/events/list
-				return gapi.client.calendar.events.list({
-					calendarId: window.semla.cid,
-					timeZone: 'Europe/London',
-					singleEvents: true,
-					timeMin: new Date().toISOString(), //gathers only events not happened yet
-					// maxResults: 20,
-					orderBy: 'startTime',
-					fields:
-						'items(summary,start,end' +
-						(window.semla.calEnhanced ? ',description,location' : '') +
-						')', // ,htmlLink
-				});
-			})
-			.then(
-				function (response) {
-					if (response.result.items) {
-						var calendarRows = ['<dl class="calendar">'];
-						var lastYearMonth = '';
-						response.result.items.forEach(function (entry) {
-							var startDate = entry.start.date
-								? entry.start.date
-								: entry.start.dateTime.substring(0, 10);
-							var yearMonth = startDate.substring(0, 8);
-							if (yearMonth !== lastYearMonth) {
-								calendarRows.push(
-									'<dt>' +
-										months[parseInt(yearMonth.substring(5, 7), 10) - 1] +
-										' ' +
-										yearMonth.substring(0, 4) +
-										'</dt>'
-								);
-								lastYearMonth = yearMonth;
-							}
-							var day = parseInt(startDate.substring(8, 10), 10);
-							var startDateDate = new Date(startDate);
-							var line = days[startDateDate.getDay()] + ' ' + day + ordinal(day);
-							var endDate;
-							if (entry.start.dateTime) {
-								line += ', ';
-								var startTime = parseTime(entry.start.dateTime);
-								var endTime = parseTime(entry.end.dateTime);
-								endDate = entry.end.dateTime.substring(0, 10);
-								if (startDate === endDate) {
-									if (startTime.ampm === endTime.ampm) {
-										startTime.ampm = '';
-									}
-									line +=
-										startTime.time +
-										startTime.ampm +
-										'-' +
-										endTime.time +
-										endTime.ampm;
-								} else {
-									line +=
-										startTime.time +
-										startTime.ampm +
-										' - ' +
-										formatEndDate(startDate, endDate) +
-										', ' +
-										endTime.time +
-										endTime.ampm;
-								}
-							} else {
-								var date = new Date(entry.end.date);
-								// subtract 1 day, or 24 * 60 * 60 * 1000 seconds
-								date.setTime(date.getTime() - 86400000);
-								endDate = date.toISOString().split('T')[0];
-								if (endDate !== startDate) {
-									line += ' - ' + formatEndDate(startDate, endDate);
-								}
-							}
-							var summary = entry.summary;
-							var mark = '';
-							if (window.semla.calEnhanced) {
-								var split = summary.split(':');
-								if (split.length > 1) {
-									summary = split[0].trim();
-									mark = ' <mark class="tag';
-									var tag = split[1].trim();
-									if (window.semla.calTags && window.semla.calTags[tag]) {
-										mark += ' semla__tag' + window.semla.calTags[tag];
-									}
-									mark += '">' + tag + '</mark>';
-								}
-							}
-							// entry.description and location can only be set if we are in enhanced mode
-							if (entry.description) {
-								var url = extractLink(entry.description);
-								if (url) {
-									summary = '<a href="' + url + '">' + summary + '</a>';
-								}
-							} else {
-								summary = '<b>' + summary + '</b>';
-							}
-							line += ' - ' + summary;
-							if (entry.location) {
-								line += ' ' + entry.location;
-							}
-							line += mark;
-							calendarRows.push('<dd>' + line + '</dd>');
-						});
-						calendarRows.push('</dl>');
-						replaceDiv(calendarRows.join(''));
-					}
-				},
-				function (reason) {
-					replaceDiv('<p>Error: ' + reason.result.error.message + '</p>');
-				}
-			);
-	}
-
-	// Loads the JavaScript client library and invokes the calendar function afterwards.
-	gapi.load('client', calendar);
 })();
