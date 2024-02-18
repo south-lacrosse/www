a7da1343bff76e2ce83c828261e77c7960b33a39
Dave Slaughter <26726004+dave-slaughter@users.noreply.github.com>
2024-02-18 19:34:06 +0000

    show all revisions in WP-CLI monitor posts
    - also pages edited with Gutenberg do not save the modified author meta
      data correctly, but revisions save the correct revision author

--- wp-content/plugins/semla/core/CLI/Monitor_Command.php
+++ wp-content/plugins/semla/core/CLI/Monitor_Command.php
@@ -44,7 +44,7 @@ class Monitor_Command {
 
 		$assoc_args = array_merge( [
 			'fields' =>
-				'ID,post_title,post_type,post_status,post_name,post_modified_gmt,post_author,modified_author',
+				'ID,post_title,post_type,post_status,post_name,post_modified_gmt,post_author,user_login',
 			'format' => 'table',
 		], $assoc_args );
 
@@ -64,18 +64,21 @@ class Monitor_Command {
 			. $assoc_args['end'] . ' GMT';
 		$email = WP_CLI\Utils\get_flag_value( $assoc_args, 'email', false );
 
-		$select = $from = '';
+		$select = $from = $where = '';
 		if ( 'ids' !== $assoc_args['format']
-		&& str_contains($assoc_args['fields'], 'modified_author')) {
-			$select = ', meta_value AS modified_author';
-			$from = "LEFT OUTER JOIN $wpdb->postmeta ON post_id = ID and meta_key = '_edit_last'";
+		&& str_contains($assoc_args['fields'], 'user_login')) {
+			$select = ', user_login';
+			$from = ", $wpdb->users AS u";
+			$where = ' AND u.ID = post_author';
 		}
+
 		$rows = $wpdb->get_results($wpdb->prepare(
-			"SELECT ID, post_title, post_type, post_status, post_name, post_modified_gmt, post_author$select
-			FROM $wpdb->posts $from
-			WHERE post_type IN ('post','page','clubs')
-			AND post_modified_gmt BETWEEN %s AND %s
-			ORDER BY post_modified_gmt", $assoc_args['start'], $assoc_args['end']));
+			"SELECT p.ID, post_title, post_type, post_status, post_name, post_modified_gmt, post_author$select
+			FROM $wpdb->posts AS p$from
+			WHERE post_type IN ('post','page','clubs','revision')
+			AND NOT (post_status = 'inherit' AND post_name LIKE '%autosave%')
+			AND post_modified_gmt BETWEEN %s AND %s $where
+			ORDER BY post_modified_gmt, p.ID", $assoc_args['start'], $assoc_args['end']));
 
 		if ($email) {
 			ob_start();
