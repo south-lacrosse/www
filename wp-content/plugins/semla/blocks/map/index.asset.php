e91d60ce14ddb1ba443d229609b3e58c573e6bfc
Dave Slaughter <26726004+dave-slaughter@users.noreply.github.com>
2025-04-15 06:08:44 +0100

    remove deprecated ButtonGroup from location block

--- wp-content/plugins/semla/blocks/map/index.asset.php
+++ wp-content/plugins/semla/blocks/map/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => 'e704ee232675d951b2af');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => 'a1e7e2ca38fd2938ccc6');
