0d253a4bfd10f0db995ef67cd1a609e05098144e
Dave Slaughter <26726004+dave-slaughter@users.noreply.github.com>
2025-07-31 20:37:04 +0100

    remove Google map iframe from map block in editor

--- wp-content/plugins/semla/blocks/map/index.asset.php
+++ wp-content/plugins/semla/blocks/map/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => 'a1e7e2ca38fd2938ccc6');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-editor', 'wp-element', 'wp-primitives'), 'version' => 'b23f2b3abaa86e0756ea');
