bf6c3116ac1e85a6500672971fa43b354fff37ac
Dave Slaughter <26726004+dave-slaughter@users.noreply.github.com>
2025-07-31 17:39:32 +0100

    add preview button to calendar block

--- wp-content/plugins/semla/blocks/calendar/index.asset.php
+++ wp-content/plugins/semla/blocks/calendar/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => 'e59194525f52c5e1d6f9');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-editor'), 'version' => 'a895aca8df465c24ff7f');
