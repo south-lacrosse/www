7a38860d7b0d63ae24a6e41293f36f7aebca875c
Dave Slaughter <26726004+dave-slaughter@users.noreply.github.com>
2025-09-03 14:21:19 +0100

    fix toc block to handle html tags in heading

--- wp-content/plugins/semla/blocks/toc/index.asset.php
+++ wp-content/plugins/semla/blocks/toc/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element'), 'version' => 'bc1a1b8ad48234179ef0');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element'), 'version' => 'bd41ded41dd692218ab4');
