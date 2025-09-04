5e61aa14392340fc30b5cbd060e9dc3510c35c2d
Dave Slaughter <26726004+dave-slaughter@users.noreply.github.com>
2025-09-04 12:31:50 +0100

    fix toc to correctly handle entities
    - fix to commit 7a38860 which incorrectly displayed & in the toc list
      item links instead of &amp;

--- wp-content/plugins/semla/blocks/toc/index.asset.php
+++ wp-content/plugins/semla/blocks/toc/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element'), 'version' => 'bd41ded41dd692218ab4');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element'), 'version' => '0673fb129277d03033e3');
