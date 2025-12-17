4a6fb81083b5a47868728b73a057b574eccae2ec
Dave Slaughter <26726004+dave-slaughter@users.noreply.github.com>
2025-12-17 21:11:02 +0000

    add CLI command to purge autosaves

--- wp-content/plugins/semla/core/CLI/Purge_Command.php
+++ wp-content/plugins/semla/core/CLI/Purge_Command.php
@@ -1,7 +1,7 @@
 <?php
 namespace Semla\CLI;
 /**
- * Purge a SEMLA cache.
+ * Purge a SEMLA cache or WordPress autosaves.
  *
  * You might want to clear the current and history caches through the SEMLA
  * Admin page as that will also do a minimal purge of Litespeed Cache entries,
@@ -19,6 +19,43 @@ use Semla\Cache;
 use \WP_CLI;
 class Purge_Command {
 	/**
+	 * Cleanup WordPress autosaves over 7 days old.
+	 */
+	public function autosaves() {
+		global $wpdb;
+
+		$old_posts = $wpdb->get_col(
+			"SELECT ID FROM $wpdb->posts
+			WHERE post_type = 'revision'
+			AND post_name REGEXP '^\\\\d+-autosave-v1$'
+			AND DATE_SUB( NOW(), INTERVAL 7 DAY ) > post_date" );
+		Util::db_check();
+
+		$count = count($old_posts);
+		if ( ! $count ) {
+			WP_CLI::warning( 'No autosaves found.' );
+			return;
+		}
+
+		WP_CLI::log("Found $count autosave(s) to delete");
+
+		$number = $successes = 0;
+		foreach ( (array) $old_posts as $ID ) {
+			$number++;
+			$progress = "$number/$count";
+			$post = wp_delete_post_revision( $ID );
+			if ($post) {
+				$successes++;
+				WP_CLI::log("$progress Autosave ID $ID ($post->post_name, $post->post_title) deleted.");
+			} else {
+				WP_CLI::warning("$progress Autosave ID $ID delete failed.");
+			}
+		}
+
+		WP_CLI::success( "$successes of $count autosave(s) deleted.");
+	}
+
+	/**
 	 * Purge current fixtures cache.
 	 */
 	public function current() {
@@ -35,7 +72,7 @@ class Purge_Command {
 	}
 
 	/**
-	 * Purge current fixtures cache.
+	 * Purge menu caches.
 	 */
 	public function menu() {
 		if (!current_theme_supports('semla')) {
