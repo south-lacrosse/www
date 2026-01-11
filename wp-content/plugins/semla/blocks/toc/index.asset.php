9a8474eac7f42aee31599c7a2afb723a23ea103d
Dave Slaughter <26726004+dave-slaughter@users.noreply.github.com>
2026-01-11 14:48:26 +0000

    change table of contents heading to h2
    - previous h4 was bad practice

--- wp-content/plugins/semla/blocks/toc/index.asset.php
+++ wp-content/plugins/semla/blocks/toc/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => '504502c763255827050c');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => '50ef7dc8ee8321af0022');
