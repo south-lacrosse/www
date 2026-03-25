67aba9b049b93cfcd58187183b07efa4cf066561
Dave Slaughter <26726004+dave-slaughter@users.noreply.github.com>
2026-03-25 21:09:58 +0000

    set map dialog isolation to the same as editor
    - upcoming versions of Gutenberg will isolate the main editor window to
      enable media processing in the browser. This also means non-isolated
      iframes cannot access the parent windows properties, which our map
      dialog needs to do, so it is set to the same isolation.
    
    Note: this was originally slated for inclusion in WordPress 7.0, but it
    looks like it will now be a later version.

--- wp-content/plugins/semla/blocks/map/index.asset.php
+++ wp-content/plugins/semla/blocks/map/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => '95c2ad041470fa293e3e');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => '67f2f7d37d60f5d7e511');
